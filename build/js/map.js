4482a34783e78abc779085a5e4ad198022e2200c
Aleksey <a.abrosimow2012@yandex.ru>
2021-05-11 13:56:06 +0300

    Правки и исправление после проверки

--- build/js/map.js
+++ build/js/map.js
@@ -1,17 +1,17 @@
 'use strict'
 
 function init() {
-  var e = new ymaps.Map("map",{
+  var e = new ymaps.Map('map',{
     center: [59.93863106, 30.3230545],
     zoom: 16
   },{
-    searchControlProvider: "yandex#search"
+    searchControlProvider: 'yandex#search'
   })
     , a = new ymaps.Placemark([59.93863106, 30.3230545],{
-      hintContent: ""
+      hintContent: ''
   },{
-      iconLayout: "default#image",
-      iconImageHref: "",
+      iconLayout: 'default#image',
+      iconImageHref: '',
       iconImageSize: [67, 100],
       iconImageOffset: [-40, -100]
   });
--- source/js/main.js
+++ source/js/main.js
@@ -53,4 +53,23 @@
       header.style.marginTop = '0';
     }
   });
+
+  function init() {
+    var e = new ymaps.Map('map',{
+      center: [59.93863106, 30.3230545],
+      zoom: 16
+    },{
+      searchControlProvider: 'yandex#search'
+    })
+      , a = new ymaps.Placemark([59.93863106, 30.3230545],{
+        hintContent: ''
+    },{
+        iconLayout: 'default#image',
+        iconImageHref: '',
+        iconImageSize: [67, 100],
+        iconImageOffset: [-40, -100]
+    });
+    e.geoObjects.add(a);
+  }
+  ymaps.ready(init)
 })();
